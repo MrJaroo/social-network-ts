9d97d67e1dc1444a91b83ab25e2356c0170064ea
MrJaroo <mishadolininma@mail.ru>
2022-01-10 17:50:43 +0400

    'fdsf'

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img className='headerImg'
+                 src="https://www.nicepng.com/png/detail/417-4170762_streamvpg-world-vector-logo-png.png"
+                 alt="cricl"/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://img1.goodfon.ru/original/800x480/e/8e/mishki-v-lesu-medvedi-les.jpg" alt="bear"/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
